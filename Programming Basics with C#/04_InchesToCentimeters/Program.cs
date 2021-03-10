341c3db888806c243e39edf349b8ef7dd65ecf05
BozhidarIvanovvv <68963017+BozhidarIvanovvv@users.noreply.github.com>
2021-03-10 18:55:27 +0200

    Add files via upload

---
+++ Programming Basics with C#/04_InchesToCentimeters/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _04_InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+            Console.WriteLine(input * 2.54);
+        }
+    }
+}
