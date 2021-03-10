1475988a14cb0c18e253059da55fc8827311b1c9
BozhidarIvanovvv <68963017+BozhidarIvanovvv@users.noreply.github.com>
2021-03-10 18:57:10 +0200

    Add files via upload

---
+++ Programming Basics with C#/07_ProjectsCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07_ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int projectsCount = int.Parse(Console.ReadLine());
+
+            int timeToMakeOneProject = 3;
+            Console.WriteLine($"The architect {name} will need {projectsCount * timeToMakeOneProject} hours to complete {projectsCount} project/s.");
+        }
+    }
+}
