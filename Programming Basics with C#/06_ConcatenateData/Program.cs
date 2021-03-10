ff0a735445b21e8e6061bb6727032f1442fef60b
BozhidarIvanovvv <68963017+BozhidarIvanovvv@users.noreply.github.com>
2021-03-10 18:56:27 +0200

    Add files via upload

---
+++ Programming Basics with C#/06_ConcatenateData/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06_ConcatenateData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fisrtName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+            Console.WriteLine($"You are {fisrtName} {lastName}, a {age}-years old person from {town}.");
+        }
+    }
+}
