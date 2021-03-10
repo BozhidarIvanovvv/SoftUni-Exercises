f42f6b57bb327bc9d18686b183abd63c6a2c9d01
BozhidarIvanovvv <68963017+BozhidarIvanovvv@users.noreply.github.com>
2021-03-10 18:57:43 +0200

    Add files via upload

---
+++ Programming Basics with C#/08_PetShop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08_PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogsCount = double.Parse(Console.ReadLine());
+            double otherAnimalsCount = double.Parse(Console.ReadLine());
+
+            double dogFoodPrice = 2.50;
+            double otherAnimalsPrice = 4.00;
+
+            double sum = 0;
+            sum += (dogFoodPrice * dogsCount) + (otherAnimalsPrice * otherAnimalsCount);
+
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
