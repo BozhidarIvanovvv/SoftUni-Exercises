ed49329ed12b0d6108c0ab53f391c1287bb5eda5
BozhidarIvanovvv <68963017+BozhidarIvanovvv@users.noreply.github.com>
2021-03-10 18:58:16 +0200

    Add files via upload

---
+++ 09_YardGreening/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace _09_YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceForOneMeter = 7.61;
+
+            double meters = double.Parse(Console.ReadLine());
+
+            double sum = meters * priceForOneMeter;
+
+            double discount = sum * 0.18;
+
+            double finalSum = sum - discount;
+
+            Console.WriteLine($"The final price is: {finalSum} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
